821c96d615a5b40626d165248601a62a7554afe6
Luigi Cruz <luigifcruz@icloud.com>
2019-01-26 04:00:06 -0200

    LRPT JPEG Decoder Initial Paser

--- gui/src/components/FilePicker.js
+++ gui/src/components/FilePicker.js
@@ -10,14 +10,22 @@ class FilePicker extends Component {
         return (
             <div className="View">
                 <div className="Header">
-                    <h1 className="Title">Select the satellite frequency band...</h1>
+                    <h1 className="Title">Choose the input format...</h1>
                     <h2 className="Description">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut. 
                     </h2>
                 </div>
                 <div className="Body">
-                    pick file name pls
-                    <Link to="decoder">Continue...</Link>
+                    <div className="Satellite">
+                        <h3>Soft-Symbol File</h3>
+                        <h4>Demodulator output with interleaved 8-bit soft-symbols.</h4>
+                        <Link to="decoder" className="Band">Browse File</Link>
+                    </div>
+                    <div className="Satellite">
+                        <h3>Decoded File</h3>
+                        <h4>Pre-processed output file from WeatherDump decoder.</h4>
+                        <Link to="decoders" className="Band">Browse File</Link>
+                    </div>
                 </div>
             </div>
         )
--- src/Meteor/Sensor/Sensor.go
+++ src/Meteor/Sensor/Sensor.go
@@ -3,20 +3,23 @@ package Sensor
 import (
 	"encoding/binary"
 	"fmt"
+	"os"
+	"reflect"
 )
 
 const sensorDataMinimum = 13
 
 type Sensor struct {
-	day  uint16
-	msec uint32
-	usec uint16
-	MCUN uint8
-	QT   uint8
-	DC   uint8
-	AC   uint8
-	QFM  uint16
-	QF   uint8
+	day     uint16
+	msec    uint32
+	usec    uint16
+	MCUN    uint8
+	QT      uint8
+	DC      uint8
+	AC      uint8
+	QFM     uint16
+	QF      uint8
+	payload []byte
 }
 
 func NewSensor(buf []byte) *Sensor {
@@ -40,6 +43,53 @@ func (e *Sensor) FromBinary(dat []byte) {
 	e.AC = uint8(dat[10]) & 0x0F
 	e.QFM = binary.BigEndian.Uint16(dat[11:])
 	e.QF = uint8(dat[13])
+
+	e.payload = dat[14:]
+}
+
+type Mask struct {
+	code []bool
+	len  int
+}
+
+var dcCategories = [12]Mask{
+	Mask{[]bool{false, false}, 3},                                           // 00
+	Mask{[]bool{true, false, true}, 4},                                      // 101
+	Mask{[]bool{true, true, false}, 5},                                      // 110
+	Mask{[]bool{true, false, false}, 2},                                     // 100
+	Mask{[]bool{false, true, true}, 1},                                      // 011
+	Mask{[]bool{false, true, false}, 0},                                     // 010
+	Mask{[]bool{true, true, true, false}, 6},                                // 1110
+	Mask{[]bool{true, true, true, true, false}, 7},                          // 11110
+	Mask{[]bool{true, true, true, true, true, false}, 8},                    // 111110
+	Mask{[]bool{true, true, true, true, true, true, false}, 9},              // 1111110
+	Mask{[]bool{true, true, true, true, true, true, true, false}, 10},       // 11111110
+	Mask{[]bool{true, true, true, true, true, true, true, true, false}, 11}, // 111111110
+}
+
+func findCategory(dat []bool) []bool {
+	for _, m := range dcCategories {
+		if reflect.DeepEqual(dat[:len(m.code)], m.code) {
+			fmt.Println(m.len)
+			return dat[len(m.code)+m.len:]
+		}
+	}
+	return nil
+}
+
+func convertToArray(buf []byte) []bool {
+	var soft = make([]bool, len(buf)*8)
+	for i, m := range buf {
+		soft[0+8*i] = m>>7&0x01 == 0x01
+		soft[1+8*i] = m>>6&0x01 == 0x01
+		soft[2+8*i] = m>>5&0x01 == 0x01
+		soft[3+8*i] = m>>4&0x01 == 0x01
+		soft[4+8*i] = m>>3&0x01 == 0x01
+		soft[5+8*i] = m>>2&0x01 == 0x01
+		soft[6+8*i] = m>>1&0x01 == 0x01
+		soft[7+8*i] = m>>0&0x01 == 0x01
+	}
+	return soft
 }
 
 func (e Sensor) Print() {
@@ -55,4 +105,11 @@ func (e Sensor) Print() {
 	fmt.Printf("Quality Factor Marker: %16b\n", e.QFM)
 	fmt.Printf("Quality Factor: %08b\n", e.QF)
 	fmt.Println()
+
+	fmt.Printf("%08b %08b\n", e.payload[0], e.payload[1])
+
+	g := convertToArray(e.payload[0:2])
+	g = findCategory(g)
+
+	os.Exit(0)
 }
